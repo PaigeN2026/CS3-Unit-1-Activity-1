6f9ed227ccdae3dbc08af109dbabc9ce80accb09
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-09-15 16:38:17 +0000

    start part a and part b

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+#  DEFINE YOUR FUNCTIONS HERE
+
+# PART A: Functions with no argument + no return
+def welc_msg():
+    print("Welcome to cooking with Paige!")
+    print("Today we will be making a caprese sandwhich 🥪")
+
+# PART B: Functions with arguments + no return 
+def store_bread(choosen_bread):
+    print(f"You're in a bakery! 🥖")
+    print(f"You have to choose which bread you want for for your sandwhich.")
+    print(f"As you walk through, you see baguettes, rolls, and so much more.")
+    print(f"You spend a lot of time pondering, but ultimately pink {choosen_bread}.")
+
+def toast_bread():
+    print(f"You take {}")
+
 def main():
     print("hello world")
-
+    # CAL YOUR FUNCTIONS in the main
 
 if __name__ == "__main__":
     main()
