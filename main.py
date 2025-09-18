9abbd45145a60d25abf1b8b2e899517cd80d93f4
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-09-18 14:30:08 +0000

    worked on stored bread

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def store_bread(choosen_bread):
     print(f"You're in a bakery! 🥖")
     print(f"You have to choose which bread you want for for your sandwhich.")
     print(f"As you walk through, you see baguettes, rolls, and so much more.")
-    print(f"You spend a lot of time pondering, but ultimately pink {choosen_bread}.")
+    print(f"You spend a lot of time pondering, but ultimately pick {choosen_bread}.")
+    
 
 def toast_bread():
-    print(f"You take {}")
+    print(f"Take your bread and cut it in half")
 
 def main():
     print("hello world")
+    
+    bread = "ciabatta"
+    store_bread(bread)
     # CAL YOUR FUNCTIONS in the main
 
 if __name__ == "__main__":
